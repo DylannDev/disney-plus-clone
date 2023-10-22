6c3472399eefd46f45e58ea132b37ae66a10e64f
dylann97 <d.xavero@hotmail.com>
2023-10-22 21:11:04 +0200

    first commit

--- app/home/page.js
+++ app/home/page.js
@@ -1,23 +1,19 @@
 "use client";
-import { useEffect, useState, React } from "react";
+
 import Header from "../Components/Header";
 import Slider from "../Components/Slider";
 import ProductionHouse from "../Components/ProductionHouse";
 import GenreMovieList from "../Components/GenreMovieList";
 import NavBarPhone from "../Components/NavBarPhone";
-import { UserAuth } from "../context/AuthContext";
 import { useRouter } from "next/navigation";
-import Login from "../Components/Login";
 
 function Home() {
-  const { user, isRedirected, setIsRedirected } = UserAuth();
+  const { user, setIsRedirected } = UserAuth();
   const router = useRouter();
 
   if (!user) {
-    // user is signed out or still being checked.
-    // don't render anything
     setIsRedirected(true);
-    router.push("/");
+    router.push("/login");
     return null;
   }
 
