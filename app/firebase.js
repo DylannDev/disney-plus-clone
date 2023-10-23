673a043bd79130eb727ff938c3359fa2cf0ef4c9
dylann97 <d.xavero@hotmail.com>
2023-10-23 02:07:14 +0200

    first commit

--- app/firebase.js
+++ app/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhwwmMVdbx12p_yHgmJsKit0Ib2L5Qjho",
-  authDomain: "disney-plus-clone-f3b53.firebaseapp.com",
-  projectId: "disney-plus-clone-f3b53",
-  storageBucket: "disney-plus-clone-f3b53.appspot.com",
-  messagingSenderId: "1063948274872",
-  appId: "1:1063948274872:web:aba3f0ddc4b341dccd784d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
